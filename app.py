711d41a480f4bea655880ec14daf9e9fa16979d9
allstrategyuser1-stack <allstrategyuser1@gmail.com>
2026-04-08 11:05:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import streamlit as st
-import pandas as pd
-import random
-from datetime import datetime, timedelta
+from generator import gerar_movimentacoes
 
 st.title("Gerador de Movimentações Financeiras")
 
 qtd = st.number_input("Quantidade de registros", 1, 1000, 100)
 
 if st.button("Gerar CSV"):
-    dados = []
-
-    for _ in range(qtd):
-        tipo = random.choice(["entrada", "saida"])
-        valor = round(random.uniform(10, 1000), 2)
-
-        data = datetime.now() - timedelta(days=random.randint(0, 365))
-
-        dados.append({
-            "data": data.strftime("%Y-%m-%d"),
-            "tipo": tipo,
-            "valor": valor
-        })
-
-    df = pd.DataFrame(dados)
+    df = gerar_movimentacoes(qtd)
 
     csv = df.to_csv(index=False).encode("utf-8")
 
