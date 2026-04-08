9cd69966c24ce5bb9b98e3f7fec892e053e3abd9
allstrategyuser1-stack <allstrategyuser1@gmail.com>
2026-04-08 11:38:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ st.title("Gerador de Movimentações Financeiras")
 
 qtd = st.number_input("Quantidade de registros", 1, 10000, 100)
 
+decimais = st.slider("Casas decimais do valor", 2, 6, 2)
+
 if st.button("Gerar CSV"):
-    df = gerar_movimentacoes(qtd)
+    df = gerar_movimentacoes(qtd, decimais)
+
+    st.dataframe(df.head())
 
     csv = df.to_csv(index=False).encode("utf-8")
 
